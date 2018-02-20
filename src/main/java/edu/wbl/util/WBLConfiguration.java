54250fa3333b7a9e83abb0d786cec08c1e001dd1
WBL-Training <training@whitebox-learning.com>
2018-02-19 21:50:23 -0800

    Update WBLConfiguration.java

--- src/main/java/edu/wbl/util/WBLConfiguration.java
+++ src/main/java/edu/wbl/util/WBLConfiguration.java
@@ -1,5 +1,7 @@
 package edu.wbl.util;
 
+import java.util.Properties;
+
 public class WBLConfiguration {
 	
 	private String browser;
@@ -8,18 +10,28 @@ public class WBLConfiguration {
 	
 	private static WBLConfiguration config=null;
 	
-	public static WBLConfiguration loadConfiguration() {
+	public static WBLConfiguration loadConfiguration(String type) throws IOException{
 		if(config != null) {
 			config = new WBLConfiguration();
 			Properties props = new Properties();
-			//if()
-			
+			props.load(new FileReader(new File("config.properties"))
+		        config.set_url(props.getProperty("url"));
+		        switch(props.getProperty("browser").toLowerCase()){
+				case "chrome" : config.set_browser(Browser.CHROME);
+				                break;
+				case "ie" : config.set_browser(Browser.IE);
+				                break;
+				case "firefox" : config.set_browser(Browser.FIREFOX);
+				                break;
+			}
 		}
-		
+		return config;
 	}
 	
 	private WBLConfiguration() {
-		
+		_url = "";
+		_browser = Browser.CHROME;
+		_device = Device.DESKTOP;
 	}
 	
 	public String get_url() {
@@ -46,5 +58,4 @@ public class WBLConfiguration {
 		this._device = _device;
 	}
 
-	
 }
